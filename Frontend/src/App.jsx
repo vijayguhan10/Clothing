3f02e53bd5893ad364794af48ab1a8bd1a5d1c46
GowthamVallarasuK <gowthamvallarasu.k2023aiml@sece.ac.in>
2024-11-07 22:36:10 +0530

    commited

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-const App = () => {
+import Nav from './components/Nav';
+
+function App(){
   return (
-    <div className="bg-red-500">
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        
+    <>
+      <Nav/>
+    </>
     
-    
-    </div>
   )
 }
 
---
+++ Frontend/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+       <div className="container mx-auto">
+          <h1 className=" font-serif text-3xl ">S O<spam className="text-blue-400"> K </spam>K A</h1>
+          
+          
+
+       </div>
+    
+
+      
+    </div>
+  )
+}
+
+export default Nav
